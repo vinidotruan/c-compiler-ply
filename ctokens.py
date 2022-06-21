9d48b629a672b597be2f367ba0510f42e9d1ce82
Ruan Vinícius <contatoruanvinicius@gmail.com>
2022-06-21 20:02:49 -0300

    - parte 2

--- ctokens.py
+++ ctokens.py
@@ -4,13 +4,43 @@ reserved = {
 	'float' : 'FLOAT',
 	'char' : 'CHAR',
 	'int' : 'INT',
+	'switch': 'SWITCH',
+	'case': 'CASE',
+	'if': 'IF',
+	'else': 'ELSE',
+	'break': 'BREAK',
 }
-tokens = list(reserved.values())+['ID','TYPEID', 'COMMA', 'SEMI', 'NUMBER', 'LBRACKET', 'RBRACKET',]
+tokens = list(reserved.values())+[
+	'ID', 'TYPEID', 'COMMA', 'SEMI', 'LBRACKET', 'RBRACKET','LCBRACKET', 'RCBRACKET','LPAR',
+	'RPAR','PLUS', 'MINUS','TIMES','DIVIDE','NAME','NUMBER',
+	'GTH', 'LTH', 'GTHOREQUAL', 'LTHOREQUAL', 'EQUALEQUAL', 'NOTEQUAL', 'DPOINT'
+]
 
 t_COMMA = '\,'
 t_SEMI = '\;'
 t_LBRACKET = '\['
+t_LCBRACKET = '\{'
 t_RBRACKET = '\]'
+t_RCBRACKET = '\}'
+t_LPAR = '\('
+t_RPAR = '\)'
+t_DPOINT = '\:'
+t_SWITCH = r'switch'
+t_CASE = r'case'
+t_IF = r'if'
+t_ELSE = r'else'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+
+t_GTH = r'\>'
+t_LTH = r'\<'
+t_GTHOREQUAL = r'\>='
+t_LTHOREQUAL = r'\<='
+t_EQUALEQUAL = r'\=='
+t_NOTEQUAL = r'\!='
+t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
 def t_ID(t):
 	r'[A-Za-z_][A-Za-z0-9_]*'
@@ -21,10 +51,20 @@ def t_NUMBER(t):
     r'\d+'
     return t
 
-t_ignore = ' \t'
+
+t_ignore = " \t"
+
+ 
+def t_nl(t):
+	r'(\n|\r|\r\n)|\s|\t'
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
 
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
+
 
 lexer = lex.lex()
--- ctokens_parse.py
+++ ctokens_parse.py
@@ -3,15 +3,127 @@ import ply.yacc as yacc
 from ctokens import tokens
 from ctokens import lexer
 
-# int teste;
-# [INTEGER] [teste] [;]
-
 def p_declaration(p):
 	'''
 	declaration : token identifier_list final_line_identifier
+				| if 
+				| else
+				| switch
+				| case
 	'''
-	
-	p[0] = p[1]+' '+p[2]+p[3]
+	if len(p) == 2:
+		p[0] = p[1]
+	else:
+		p[0] = p[1]+str(p[2])+p[3]
+
+def p_if(p):
+	'''
+	if : IF comparison command final_line_identifier
+		| if_withou_else
+	'''
+	if len(p) == 2:
+		p[0] = p[1]
+	else:
+		p[0] = p[1]+str(p[2])+str(p[3])
+
+def p_if_withou_else(p):
+	'''
+	if_withou_else : IF comparison command 
+	'''
+	p[0] = p[1]+str(p[2])+str(p[3]);
+
+def p_if_else(p):
+	'''
+	else : if_withou_else ELSE command final_line_identifier
+	'''
+	p[0] = p[1]+p[2]+str(p[3]);
+
+def p_switch(p):
+	'''
+	switch : SWITCH LPAR identifier RPAR LCBRACKET case RCBRACKET
+	'''
+	p[0] = p[1]+p[2]+p[3]+p[4]+p[5]+p[6]
+
+def p_case(p):
+	'''
+	case : CASE DPOINT command_case
+	'''
+	p[0] = p[1]+p[2]+str(p[3])
+
+def p_command(p):
+	'''
+	command : LCBRACKET NUMBER MINUS NUMBER final_line_identifier RCBRACKET
+			| LCBRACKET NUMBER PLUS NUMBER final_line_identifier RCBRACKET
+			| LCBRACKET NUMBER TIMES NUMBER final_line_identifier RCBRACKET
+			| LCBRACKET NUMBER DIVIDE NUMBER final_line_identifier RCBRACKET
+	'''
+	if p[3] == '-':
+		p[0] = p[1]+str(p[2]-p[4])+p[5]+p[6]
+	elif p[3] == '+':
+		p[0] = p[1]+str(p[2]+p[4])+p[5]+p[6]
+	elif p[3] == '/':
+		p[0] = p[1]+str(int(p[2])/int(p[4]))+p[5]+p[6]
+	elif p[3] == '*':
+		p[0] = p[1]+str(int(p[2])*int(p[4]))+p[5]+p[6]
+	else:
+		p_error(p);
+
+def p_command_case(p):
+	'''
+	command_case : LCBRACKET NUMBER MINUS NUMBER BREAK final_line_identifier RCBRACKET
+			| LCBRACKET NUMBER PLUS NUMBER BREAK final_line_identifier RCBRACKET
+			| LCBRACKET NUMBER TIMES NUMBER BREAK final_line_identifier RCBRACKET
+			| LCBRACKET NUMBER DIVIDE NUMBER BREAK final_line_identifier RCBRACKET
+	'''
+	p[0] = f'{p[1]+p[2]+p[3]+p[4]+p[5]+p[6]+p[7]}'
+
+
+
+def p_comparison(p):
+	'''
+	comparison : LPAR NUMBER GTH NUMBER RPAR
+				| LPAR NUMBER LTH NUMBER RPAR
+                | LPAR NUMBER GTHOREQUAL NUMBER RPAR
+                | LPAR NUMBER LTHOREQUAL NUMBER RPAR
+                | LPAR NUMBER EQUALEQUAL NUMBER RPAR
+                | LPAR NUMBER NOTEQUAL NUMBER RPAR
+	'''
+	if p[3] == '>':
+		if p[2] > p[4]:
+			p[0] = True
+		else:
+			p[0] = False
+
+	elif p[3] == '<':
+		if p[2] > p[4]:
+			p[0] = True
+		else:
+			p[0] = False
+
+	elif p[3] == '>=':
+		if p[2] > p[4]:
+			p[0] = True
+		else:
+			p[0] = False
+
+	elif p[3] == '<=':
+		if p[2] > p[4]:
+			p[0] = True
+		else:
+			p[0] = False
+
+	elif p[3] == '==':
+		if p[2] > p[4]:
+			p[0] = True
+		else:
+			p[0] = False
+
+	elif p[3] == '!=':
+		if p[2] > p[4]:
+			p[0] = True
+		else:
+			p[0] = False 
+
 
 def p_identifier_list(p):
 	'''
@@ -57,14 +169,15 @@ def p_semi(p):
 	p[0] = p[1]
 
 def p_error(p):
-	print('Digitou algo errado!', p)
+	print(f'Digitou algo errado! {p}')
+
 
 parser = yacc.yacc()
 
-
-while True:
+arquivo = open('arquivo_input.txt', 'r')
+for data in arquivo:
 	try:
-		s = input('digite: ')
+		s = data
 	except EOFError:
 		break
 	if not s: continue
